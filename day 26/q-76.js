c86f4955f4b978f40780f31d86090443faedb911
FaizaSiddiqu <160780189+FaizaSiddiqu@users.noreply.github.com>
2024-06-18 01:14:06 +0500

    add files

---
+++ day 26/q-76.js
@@ -0,0 +1,6 @@
+// Function Parameters and Return Values: Create a function that takes two numbers as parameters,
+//  adds them together, and returns the result. Show how you can call this function and log the result.
+function add(val1, val2) {
+    return val1 + val2;
+}
+console.log(add(3, 5));
---
+++ day 26/q-78.ts
@@ -0,0 +1,14 @@
+// Function Expressions vs. Function Declarations: Compare function expressions and declarations by creating 
+// one of each that performs the same task, such as squaring a number.
+
+function squareDeclaration(number1: number ) : number{
+    return number1 * number1
+
+}
+ let squareExpression = function( number : number){
+    return number * number
+
+ }
+ console.log(squareDeclaration(4))
+ console.log(squareExpression(4))
+ // Both methods give us the same result, showing two different ways to create functions that do the same thing.
